01c2a8af799a6cb44bbcb4c2cc5bb97f7d4e3754
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2024-04-25 01:36:24 +0300

    finish

---
+++ form.js
@@ -0,0 +1,14 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  window.location.href = "./thankyou.html";
+}
+
+// close cookie
+function closeCookiePopup() {
+  document.getElementById("cookie-popup").style.display = "none";
+}
+
+//  cookie
+setTimeout(() => {
+  document.getElementById("cookie-popup").style.display = "block";
+}, 5000);
